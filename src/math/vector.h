eb60ff69f5966fe2d1f14f17af0471c1793bdd9e
AndreyKlimenk0 <deadshot271998@gmail.com>
2020-11-20 22:31:39 +0200

    Wrote Vector2, Vector3, Vector4 and Vectex_Col

--- src/math/vector.h
+++ src/math/vector.h
@@ -2,6 +2,7 @@
 #define VECTOR_H
 
 #include <DirectXMath.h>
+#include <math.h>
 
 using namespace DirectX;
 
@@ -12,6 +13,8 @@ struct Vector2 {
 	Vector2() {};
 	Vector2(float _x, float _y) : x(_x), y(_y) {}
 
+	float  &operator[](int i);
+	const float  &operator[](int i) const;
 	Vector2 operator+(float scalar);
 	Vector2 operator+(const Vector2 &other);
 	Vector2 operator-(float scalar);
@@ -20,13 +23,32 @@ struct Vector2 {
 	Vector2 operator*(const Vector2 &other);
 	Vector2 operator/(float scalar);
 	Vector2 operator/(const Vector2 &other);
+
+	Vector2 &operator+=(float);
+	Vector2 &operator+=(const Vector2 &other);
+	Vector2 &operator-=(float);
+	Vector2 &operator-=(const Vector2 &other);
+	Vector2 &operator*=(float);
+	Vector2 &operator*=(const Vector2 &other);
+	Vector2 &operator/=(float);
+	Vector2 &operator/=(const Vector2 &other);
 	
 	operator XMVECTOR();
 
-	float length();
-	float normalize();
-
+	float   length();
+	Vector2 normalize();
+	float dot(const Vector2 & other);
 };
+
+inline float &Vector2::operator[](int i)
+{
+	return (&x)[i];
+}
+
+inline const float &Vector2::operator[](int i) const
+{
+	return (&x)[i];
+}
 
 inline Vector2 Vector2::operator+(float scalar) 
 {
@@ -50,7 +72,7 @@ inline Vector2 Vector2::operator-(const Vector2 &other)
 
 inline Vector2 Vector2::operator*(float scalar) 
 {
-	return Vector2(x * scalar, x * scalar);
+	return Vector2(x * scalar, y * scalar);
 }
 inline Vector2 Vector2::operator*(const Vector2 &other) 
 {
@@ -60,20 +82,449 @@ inline Vector2 Vector2::operator*(const Vector2 &other)
 inline Vector2 Vector2::operator/(float scalar) 
 {
 	float inver = 1.0f / scalar;
-	return Vector2(x / scalar, y / scalar);
+	return Vector2(x * inver, y * inver);
 }
 
 inline Vector2 Vector2::operator/(const Vector2 &other) 
 {
-	return Vector2(x / other.x, y / other.y);
+	float inver_x = 1.0f / other.x;
+	float inver_y = 1.0f / other.y;
+	return Vector2(x * inver_x, y * inver_y);
 }
 
-Vector2::operator XMVECTOR()
+inline Vector2 &Vector2::operator+=(float scalar)
+{
+	x += scalar;
+	y += scalar;
+	return *this;
+}
+
+inline Vector2 &Vector2::operator+=(const Vector2 &other)
+{
+	x += other.x;
+	y += other.y;
+	return *this;
+}
+
+inline Vector2 &Vector2::operator-=(float scalar)
+{
+	x -= scalar;
+	y -= scalar;
+	return *this;
+}
+
+inline Vector2 &Vector2::operator-=(const Vector2 &other)
+{
+	x -= other.x;
+	y -= other.y;
+	return *this;
+}
+
+inline Vector2 &Vector2::operator*=(float scalar)
+{
+	x *= scalar;
+	y *= scalar;
+	return *this;
+}
+
+inline Vector2 &Vector2::operator*=(const Vector2 &other)
+{
+	x *= other.x;
+	y *= other.y;
+	return *this;
+}
+
+inline Vector2 &Vector2::operator/=(float scalar)
+{
+	float inver = 1.0f / scalar;
+	x *= inver;
+	y *= inver;
+	return *this;
+}
+
+inline Vector2 &Vector2::operator/=(const Vector2 &other)
+{
+	x *= 1.0f / other.x;
+	y *= 1.0f / other.y;
+	return *this;
+}
+
+inline Vector2::operator XMVECTOR()
 {
 	return XMVectorSet(x, y, 0.0f, 0.0f);
 }
 
-struct Vector3 {};
-struct Vector4 {};
+inline float Vector2::length()
+{
+	return (float)sqrt(x * x + y * y);
+}
+
+inline Vector2 Vector2::normalize()
+{
+	return Vector2(x / length(), y / length());
+}
+
+inline float Vector2::dot(const Vector2 &other)
+{
+	return x * other.x + y * other.y;
+}
+
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+
+	Vector3() {};
+	Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+
+	float  &operator[](int i);
+	const float  &operator[](int i) const;
+	Vector3 operator+(float scalar);
+	Vector3 operator+(const Vector3 &other);
+	Vector3 operator-(float scalar);
+	Vector3 operator-(const Vector3 &other);
+	Vector3 operator*(float scalar);
+	Vector3 operator*(const Vector3 &other);
+	Vector3 operator/(float scalar);
+	Vector3 operator/(const Vector3 &other);
+
+	Vector3 &operator+=(float);
+	Vector3 &operator+=(const Vector3 &other);
+	Vector3 &operator-=(float);
+	Vector3 &operator-=(const Vector3 &other);
+	Vector3 &operator*=(float);
+	Vector3 &operator*=(const Vector3 &other);
+	Vector3 &operator/=(float);
+	Vector3 &operator/=(const Vector3 &other);
+
+	operator XMVECTOR();
+
+	float   length();
+	Vector3 normalize();
+	float dot(const Vector3 & other);
+};
+
+inline float &Vector3::operator[](int i)
+{
+	return (&x)[i];
+}
+
+inline const float &Vector3::operator[](int i) const
+{
+	return (&x)[i];
+}
+
+inline Vector3 Vector3::operator+(float scalar)
+{
+	return Vector3(x + scalar, y + scalar, z + scalar);
+}
+
+inline Vector3 Vector3::operator+(const Vector3 &other)
+{
+	return Vector3(x + other.x, y + other.y, z + other.z);
+}
+
+inline Vector3 Vector3::operator-(float scalar)
+{
+	return Vector3(x - scalar, y - scalar, z - scalar);
+}
+
+inline Vector3 Vector3::operator-(const Vector3 &other)
+{
+	return Vector3(x - other.x, y - other.y, z - other.z);
+}
+
+inline Vector3 Vector3::operator*(float scalar)
+{
+	return Vector3(x * scalar, y * scalar, z * scalar);
+}
+
+inline Vector3 Vector3::operator*(const Vector3 &other)
+{
+	return Vector3(x * other.x, y * other.y, z * other.z);
+}
+
+inline Vector3 Vector3::operator/(float scalar)
+{
+	float inver = 1.0f / scalar;
+	return Vector3(x * inver, y * inver, z * inver);
+}
+
+inline Vector3 Vector3::operator/(const Vector3 &other)
+{
+	float inver_x = 1.0f / other.x;
+	float inver_y = 1.0f / other.y;
+	float inver_z = 1.0f / other.z;
+	return Vector3(x * inver_x, y * inver_y, z * inver_z);
+}
+
+inline Vector3 &Vector3::operator+=(float scalar)
+{
+	x += scalar;
+	y += scalar;
+	z += scalar;
+	return *this;
+}
+
+inline Vector3 &Vector3::operator+=(const Vector3 &other)
+{
+	x += other.x;
+	y += other.y;
+	z += other.z;
+	return *this;
+}
+
+inline Vector3 &Vector3::operator-=(float scalar)
+{
+	x -= scalar;
+	y -= scalar;
+	z -= scalar;
+	return *this;
+}
+
+inline Vector3 &Vector3::operator-=(const Vector3 &other)
+{
+	x -= other.x;
+	y -= other.y;
+	z -= other.z;
+	return *this;
+}
+
+inline Vector3 &Vector3::operator*=(float scalar)
+{
+	x *= scalar;
+	y *= scalar;
+	z *= scalar;
+	return *this;
+}
+
+inline Vector3 &Vector3::operator*=(const Vector3 &other)
+{
+	x *= other.x;
+	y *= other.y;
+	z *= other.z;
+	return *this;
+}
+
+inline Vector3 &Vector3::operator/=(float scalar)
+{
+	float inver = 1.0f / scalar;
+	x *= inver;
+	y *= inver;
+	z *= inver;
+	return *this;
+}
+
+inline Vector3 &Vector3::operator/=(const Vector3 &other)
+{
+	x *= 1.0f / other.x;
+	y *= 1.0f / other.y;
+	z *= 1.0f / other.z;
+	return *this;
+}
+
+inline Vector3::operator XMVECTOR()
+{
+	return XMVectorSet(x, y, z, 0.0f);
+}
+
+inline float Vector3::length()
+{
+	return (float)sqrt(x * x + y * y + z * z);
+}
+
+inline Vector3 Vector3::normalize()
+{
+	return Vector3(x / length(), y / length(), z / length());
+}
+
+inline float Vector3::dot(const Vector3 &other)
+{
+	return x * other.x + y * other.y + z * other.z;
+}
+
+struct Vector4 {
+	float x;
+	float y;
+	float z;
+	float w;
+
+	Vector4() {};
+	Vector4(float _x, float _y, float _z, float _w) : x(_x), y(_y), z(_z), w(_w) {}
+
+	float  &operator[](int i);
+	const float  &operator[](int i) const;
+	Vector4 operator+(float scalar);
+	Vector4 operator+(const Vector4 &other);
+	Vector4 operator-(float scalar);
+	Vector4 operator-(const Vector4 &other);
+	Vector4 operator*(float scalar);
+	Vector4 operator*(const Vector4 &other);
+	Vector4 operator/(float scalar);
+	Vector4 operator/(const Vector4 &other);
+
+	Vector4 &operator+=(float);
+	Vector4 &operator+=(const Vector4 &other);
+	Vector4 &operator-=(float);
+	Vector4 &operator-=(const Vector4 &other);
+	Vector4 &operator*=(float);
+	Vector4 &operator*=(const Vector4 &other);
+	Vector4 &operator/=(float);
+	Vector4 &operator/=(const Vector4 &other);
+
+	operator XMVECTOR();
+
+	float   length();
+	Vector4 normalize();
+	float dot(const Vector4 & other);
+};
+
+inline float &Vector4::operator[](int i)
+{
+	return (&x)[i];
+}
+
+inline const float &Vector4::operator[](int i) const
+{
+	return (&x)[i];
+}
+
+inline Vector4 Vector4::operator+(float scalar)
+{
+	return Vector4(x + scalar, y + scalar, z + scalar, w + scalar);
+}
+
+inline Vector4 Vector4::operator+(const Vector4 &other)
+{
+	return Vector4(x + other.x, y + other.y, z + other.z, w + other.w);
+}
+
+inline Vector4 Vector4::operator-(float scalar)
+{
+	return Vector4(x - scalar, y - scalar, z - scalar, w - scalar);
+}
+
+inline Vector4 Vector4::operator-(const Vector4 &other)
+{
+	return Vector4(x - other.x, y - other.y, z - other.z, w - other.w);
+}
+
+inline Vector4 Vector4::operator*(float scalar)
+{
+	return Vector4(x * scalar, y * scalar, z * scalar, w * scalar);
+}
+
+inline Vector4 Vector4::operator*(const Vector4 &other)
+{
+	return Vector4(x * other.x, y * other.y, z * other.z, w * other.w);
+}
+
+inline Vector4 Vector4::operator/(float scalar)
+{
+	float inver = 1.0f / scalar;
+	return Vector4(x * inver, y * inver, z * inver, w * inver);
+}
+
+inline Vector4 Vector4::operator/(const Vector4 &other)
+{
+	float inver_x = 1.0f / other.x;
+	float inver_y = 1.0f / other.y;
+	float inver_z = 1.0f / other.z;
+	float inver_w = 1.0f / other.w;
+	return Vector4(x * inver_x, y * inver_y, z * inver_z, w * inver_w);
+}
+
+inline Vector4 &Vector4::operator+=(float scalar)
+{
+	x += scalar;
+	y += scalar;
+	z += scalar;
+	w += scalar;
+	return *this;
+}
+
+inline Vector4 &Vector4::operator+=(const Vector4 &other)
+{
+	x += other.x;
+	y += other.y;
+	z += other.z;
+	w += other.w;
+	return *this;
+}
+
+inline Vector4 &Vector4::operator-=(float scalar)
+{
+	x -= scalar;
+	y -= scalar;
+	z -= scalar;
+	w -= scalar;
+	return *this;
+}
+
+inline Vector4 &Vector4::operator-=(const Vector4 &other)
+{
+	x -= other.x;
+	y -= other.y;
+	z -= other.z;
+	w -= other.w;
+	return *this;
+}
+
+inline Vector4 &Vector4::operator*=(float scalar)
+{
+	x *= scalar;
+	y *= scalar;
+	z *= scalar;
+	w *= scalar;
+	return *this;
+}
+
+inline Vector4 &Vector4::operator*=(const Vector4 &other)
+{
+	x *= other.x;
+	y *= other.y;
+	z *= other.z;
+	w *= other.w;
+	return *this;
+}
+
+inline Vector4 &Vector4::operator/=(float scalar)
+{
+	float inver = 1.0f / scalar;
+	x *= inver;
+	y *= inver;
+	z *= inver;
+	w *= inver;
+	return *this;
+}
+
+inline Vector4 &Vector4::operator/=(const Vector4 &other)
+{
+	x *= 1.0f / other.x;
+	y *= 1.0f / other.y;
+	z *= 1.0f / other.z;
+	w *= 1.0f / other.w;
+	return *this;
+}
+
+inline Vector4::operator XMVECTOR()
+{
+	return XMVectorSet(x, y, z, w);
+}
+
+inline float Vector4::length()
+{
+	return (float)sqrt(x * x + y * y + z * z + w * w);
+}
+
+inline Vector4 Vector4::normalize()
+{
+	return Vector4(x / length(), y / length(), z / length(), w / length());
+}
+
+inline float Vector4::dot(const Vector4 &other)
+{
+	return x * other.x + y * other.y + z * other.z * w + other.w;
+}
 
 #endif
