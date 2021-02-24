299e4e5c0d52c2f18a2aa14bb7e4e07174b081d6
AndreyKlimenk0 <deadshot271998@gmail.com>
2021-02-24 20:08:24 +0200

    I am still trying to solver the problem with texture mapping

--- src/render/model.cpp
+++ src/render/model.cpp
@@ -2,11 +2,15 @@
 #include <d3dx11.h>
 
 #include "base.h"
+#include "mesh.h"
 #include "model.h"
 #include "../framework/file.h"
 #include "../sys/sys_local.h"
 #include "../libs/fbx_loader.h"
 #include "../libs/str.h"
+
+#include "../libs/ds/array.h"
+#include "../libs/math/vector.h"
 
 
 Model::~Model()
@@ -38,4 +42,76 @@ void Model::init_from_file(const char *file_name)
 		printf("Model::init_from_file: %s is unkown model type, now only supports fbx file type\n", file_extension);
 		return;
 	}
+}
+
+inline Vector3 tokens_to_vector3(Array<char *> *tokens)
+{
+	Vector3 v;
+	float s[3];
+	for (int i = 1; i < tokens->count; i++) {
+		s[i - 1] = (float)atof(tokens->at(i));
+		v.x = s[0];
+		v.y = s[1];
+		v.z = s[2];
+	}
+	return v;
+}
+
+void load_model_from_obj_file(const char *file_name, Triangle_Mesh *mesh)
+{
+	char *buffer = read_entire_file(file_name);
+	Array<Vector3> positions;
+	Array<Vector3> normals;
+	Array<Vector2> uvs;
+
+	while (1) {
+		char *line = get_next_line(&buffer);
+		if (!line) {
+			break;
+		}
+		if (line[0] == '#' || line[0] == 'o' || line[0] == '\0') {
+			continue;
+		}
+		Array<char *> tokens;
+		split(line, " ", &tokens);
+		if (!strcmp(tokens[0], "mtllib")) {
+			printf("file tpe mtllib ins't parsed\n");
+			continue;
+		}
+		Vertex *vertex = new Vertex();
+		if (!strcmp(tokens[0], "v")) {
+			assert(tokens.count == 4);
+			positions.push(tokens_to_vector3(&tokens));
+
+		} else if (!strcmp(tokens[0], "vt")) {
+			//assert(tokens->size() == 3);
+			uvs.push((Vector2)*((Vector2 *)&tokens_to_vector3(&tokens)));
+
+		} else if (!strcmp(tokens[0], "vn")) {
+			assert(tokens.count == 4);
+			normals.push(tokens_to_vector3(&tokens));
+
+		} else if (!strcmp(tokens[0], "f")) {
+			for (int i = 1; i < tokens.count; i++) {
+				Array<char *> vertex_indices;
+				split(tokens[i], "/", &vertex_indices);
+				int vertex_index = atoi(vertex_indices[0]);
+				int texture_index = atoi(vertex_indices[1]);
+				int normal_index = atoi(vertex_indices[2]);
+
+				Vertex vertex;
+				vertex.position = positions[vertex_index - 1];
+				vertex.uv = uvs[texture_index - 1];
+				vertex.normal = normals[normal_index - 1];
+				
+				//mesh->allocate_vertices(vertex_index - 1);
+
+				vertices->push_back(vertex);
+				indices->push_back(vertex_index);
+			}
+		} else {
+			printf("This keyworld (%s) isn't supported\n", tokens->at(0));
+		}
+	}
+
 }
