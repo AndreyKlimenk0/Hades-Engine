25df3ecd7454e703d5d1358f143986657ef67d45
AndreyKlimenk0 <deadshot271998@gmail.com>
2022-01-15 13:48:35 +0200

    Making own 2d render api

--- src/render/render_system.h
+++ src/render/render_system.h
@@ -12,28 +12,6 @@
 #include "../libs/os/camera.h"
 
 
-struct View_Info;
-
-
-struct Render_System {
-	~Render_System();
-
-	World  *current_render_world = NULL;
-	
-	View_Info *view_info = NULL;
-	Free_Camera *free_camera = NULL;
-
-	Matrix4 view_matrix;
-
-	void init(View_Info *_view_info);
-	void resize();
-	void shutdown();
-	
-	void render_frame();
-};
-
-extern Render_System render_sys;
-
 struct View_Info {
 	int window_width;
 	int window_height;
@@ -57,6 +35,81 @@ inline void View_Info::update_projection_matries()
 
 View_Info *make_view_info(float near_plane, float far_plane);
 
+enum Rect_Side;
+typedef ID3D11Buffer Gpu_Buffer;
+
+struct Primitive_2D {
+	//Vars is set by Render_2D
+	u32 vertex_offset = 0;
+	u32 index_offset = 0;
+
+	Array<Vector2> points;
+	Array<Vertex_XC> vertices;
+	Array<u32> indices;
+
+	void add_point(const Vector2 &point) { points.push(point); }
+	void add_rounded_points(int x, int y, int width, int height, Rect_Side rect_side, u32 rounding);
+	void make_triangle_polygon(const Color &color);
+};
+
+const u32 ROUND_TOP_LEFT_RECT = 0x1;
+const u32 ROUND_TOP_RIGHT_RECT = 0x4;
+const u32 ROUND_BOTTOM_LEFT_RECT = 0x2;
+const u32 ROUND_BOTTOM_RIGHT_RECT = 0x8;
+const u32 ROUND_TOP_RECT = ROUND_TOP_LEFT_RECT | ROUND_TOP_RIGHT_RECT;
+const u32 ROUND_BOTTOM_RECT = ROUND_BOTTOM_LEFT_RECT | ROUND_BOTTOM_RIGHT_RECT;
+const u32 ROUND_RECT = ROUND_TOP_RECT | ROUND_BOTTOM_RECT;
+
+struct Render_2D {
+	~Render_2D();
+
+	Gpu_Buffer *vertex_buffer = NULL;
+	Gpu_Buffer *index_buffer = NULL;
+
+	u32 total_vertex_count = 0;
+	u32 total_index_count = 0;
+
+	Array<Primitive_2D *> primitives;
+
+	void clear();
+	void add_primitive(Primitive_2D *primitive);
+	void draw_rect(int x, int y, int width, int height, const Color &color, u32 rounding = 0, u32 flags = ROUND_RECT);
+	void draw_primitives();
+};
+
+inline void Render_2D::clear()
+{
+	Primitive_2D *primitive = NULL;
+	For(primitives, primitive) {
+		DELETE_PTR(primitive);
+	}
+	
+	primitives.clear();
+	
+	total_vertex_count = 0;
+	total_index_count = 0;
+}
+
+struct Render_System {
+	~Render_System();
+
+	World  *current_render_world = NULL;
+	
+	View_Info *view_info = NULL;
+	Free_Camera *free_camera = NULL;
+
+	Matrix4 view_matrix;
+
+	Render_2D render_2d;
+
+	void init(View_Info *_view_info);
+	void resize();
+	void shutdown();
+	
+	void render_frame();
+};
+
+extern Render_System render_sys;
 
 void make_outlining(Render_Entity *render_entity);
 void free_outlining(Render_Entity *render_entity);
