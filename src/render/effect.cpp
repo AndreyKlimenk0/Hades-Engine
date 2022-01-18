e06a01f0da347fd8091b6e1a9cf7597aa1a380c1
AndreyKlimenk0 <deadshot271998@gmail.com>
2022-01-18 21:36:15 +0200

    Making own 2d render

--- src/render/effect.cpp
+++ src/render/effect.cpp
@@ -14,17 +14,6 @@
 
 Fx_Shader_Manager fx_shader_manager;
 
-
-#define VALID_VAR_NAME(var, var_name, var_type) { \
-	if (!var->IsValid()) { \
-		if (!is_var_in_not_found_vars_array(var_name)) { \
-			print("There is no the variable with name {} of type {} in {}.fx", var_name, var_type, name); \
-			not_found_vars.push(var_name); \
-		}  \
-		return; \
-	} \
-}
-
 Fx_Shader::Fx_Shader(String *shader_name, ID3DX11Effect *fx_shader)
 {
 	name = *shader_name;
@@ -35,95 +24,6 @@ Fx_Shader::Fx_Shader(String *shader_name, ID3DX11Effect *fx_shader)
 	technique_count = effect_desc.Techniques;
 }
 
-void Fx_Shader::attach(u32 technique_index, u32 pass_index)
-{
-	assert(technique_count > technique_index);
-
-	shader->GetTechniqueByIndex(technique_index)->GetPassByIndex(pass_index)->Apply(0, directx11.device_context);
-}
-
-void Fx_Shader::attach(const char *technique_name, u32 pass_index)
-{
-	assert(technique_name);
-
-	HR(shader->GetTechniqueByName(technique_name)->GetPassByIndex(pass_index)->Apply(0, directx11.device_context));
-}
-
-void Fx_Shader::bind(const char *var_name, int scalar)
-{
-	ID3DX11EffectScalarVariable * var = shader->GetVariableByName(var_name)->AsScalar();
-	VALID_VAR_NAME(var, var_name, "Scalar");
-	var->SetInt(scalar);
-}
-
-void Fx_Shader::bind(const char *var_name, Vector3 *vector)
-{
-	ID3DX11EffectVectorVariable *var = shader->GetVariableByName(var_name)->AsVector();
-	VALID_VAR_NAME(var, var_name, "Vector3");
-	var->SetRawValue(*vector, 0, sizeof(Vector3));
-}
-
-void Fx_Shader::bind(const char *var_name, Vector4 *vector)
-{
-	ID3DX11EffectVectorVariable * var = shader->GetVariableByName(var_name)->AsVector();
-	VALID_VAR_NAME(var, var_name, "Vector4");
-	var->SetFloatVector(*vector);
-}
-
-void Fx_Shader::bind(const char *var_name, Matrix4 *matrix)
-{
-	ID3DX11EffectMatrixVariable *var = shader->GetVariableByName(var_name)->AsMatrix();
-	VALID_VAR_NAME(var, var_name, "Matrix");
-	HR(var->SetMatrix(*matrix));
-}
-
-void Fx_Shader::bind(const char *var_name, ID3D11ShaderResourceView *texture)
-{
-	ID3DX11EffectShaderResourceVariable * var = shader->GetVariableByName(var_name)->AsShaderResource();
-	VALID_VAR_NAME(var, var_name, "ShaderResource");
-	HR(var->SetResource(texture));
-}
-
-void Fx_Shader::bind(const char *var_name, void *struct_ptr, u32 struct_size)
-{
-	ID3DX11EffectVariable * var = shader->GetVariableByName(var_name);
-	VALID_VAR_NAME(var, var_name, "Struct");
-	HR(var->SetRawValue(struct_ptr, 0, struct_size));
-}
-
-void Fx_Shader::bind_entity(Entity * entity, Matrix4 &view, Matrix4 &perspective, Render_Mesh *r)
-{
-	Matrix4 world = entity->get_world_matrix();
-	//Matrix4 world = r->position * r->orientation * r->scale;
-	Matrix4 wvp_projection = world * view * perspective;
-	//Matrix4 w;
-	//bind("world", &r->transform);
-	bind("world", &world);
-	bind("world_view_projection", &wvp_projection);
-
-	if (entity->type != ENTITY_TYPE_LIGHT) {
-		bind("texture_map", r->diffuse_texture->shader_resource);
-		bind("material", (void *)&r->material, sizeof(Material));
-
-	}
-}
-
-void Fx_Shader::bind_per_frame_info(Free_Camera *camera)
-{
-	bind("camera_position", &camera->position);
-	bind("camera_direction", &camera->target);
-}
-
-bool Fx_Shader::is_var_in_not_found_vars_array(const char * var_name)
-{
-	String *var = NULL;
-	For(not_found_vars, var) {
-		if (*var == var_name) {
-			return true;
-		}
-	}
-	return false;
-}
 
 Fx_Shader::~Fx_Shader()
 {
@@ -176,6 +76,22 @@ Fx_Shader *Fx_Shader_Manager::get_shader(const char *name)
 	return (*shaders)[name];
 }
 
+void Fx_Shader::bind_entity(Entity * entity, Matrix4 &view, Matrix4 &perspective, Render_Mesh *r)
+{
+	Matrix4 world = entity->get_world_matrix();
+	//Matrix4 world = r->position * r->orientation * r->scale;
+	Matrix4 wvp_projection = world * view * perspective;
+	//Matrix4 w;
+	//bind("world", &r->transform);
+	bind("world", &world);
+	bind("world_view_projection", &wvp_projection);
+
+	if (entity->type != ENTITY_TYPE_LIGHT) {
+		bind("texture_map", r->diffuse_texture->shader_resource);
+		bind("material", (void *)&r->material, sizeof(Material));
+
+	}
+}
 
 void bind_light(Fx_Shader *forward_light_shader, Array<Light *> *lights)
 {
