4351d041980d0558c5b2e9b5210054570dac8dff
Andrey <deadshot271998@gmail.com>
2023-01-23 22:08:01 +0200

    Made camera rotaion

--- src/libs/math/vector.cpp
+++ src/libs/math/vector.cpp
@@ -6,3 +6,19 @@ Vector3 base_z_vec = Vector3(0.0f, 0.0f, 1.0f);
 
 Vector2 base_x_vec2 = Vector2(1.0f, 0.0f);
 Vector2 base_y_vec2 = Vector2(0.0f, 1.0f);
+
+Vector3::Vector3(const Vector4 & vec)
+{
+	x = vec.x;
+	y = vec.y;
+	z = vec.z;
+}
+
+
+Vector3 & Vector3::operator=(const Vector4 & vec4)
+{
+	x = vec4.x;
+	y = vec4.y;
+	z = vec4.z;
+	return *this;
+}
