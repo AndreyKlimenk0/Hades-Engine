1490c74e551e084b7a13b92759d83f1729675a93
Andrey <deadshot271998@gmail.com>
2024-01-30 15:49:00 +0200

    Made refactoring in editor

--- src/collision/collision.h
+++ src/collision/collision.h
@@ -1,8 +1,9 @@
 #ifndef HADES_COLLISION_H
 #define HADES_COLLISION_H
 
-#include "../libs/math/vector.h"
 #include "../render/model.h"
+#include "../libs/math/structures.h"
+#include "../libs/math/vector.h"
 
 enum Boudning_Box_Type {
 	BOUNDING_BOX_TYPE_UNKNOWN,
@@ -23,5 +24,7 @@ struct Bounding_Sphere {
 
 AABB make_AABB(Triangle_Mesh *mesh);
 Bounding_Sphere make_bounding_sphere(const Vector3 &position, Triangle_Mesh *mesh);
+
+bool detect_intersection(Ray *ray, AABB *aabb, Vector3 *intersection_point = NULL);
 
 #endif
