acda5bee8fbc2e71f220933291eb4c5ecf3a5218
Andrey <deadshot271998@gmail.com>
2024-08-05 12:00:37 +0300

    Added functions for converting one dimensional to three dimensional index and vice versaю

--- src/gui/editor.cpp
+++ src/gui/editor.cpp
@@ -517,25 +517,6 @@ void Render_World_Window::update()
 {
 }
 
-inline Point_u32 convert_1d_to_3d_index(u32 one_dimensional_index, u32 height, u32 depth)
-{
-	u32 i = one_dimensional_index / (height * depth);
-	u32 j = (one_dimensional_index % (height * depth)) / depth;
-	u32 k = one_dimensional_index % depth;
-	return Point_u32(i, j, k);
-}
-
-u32 how_many(Array<Voxel> &voxels)
-{
-	u32 count = 0;
-	for (u32 i = 0; i < voxels.count; i++) {
-		if (voxels[i].occlusion != 0) {
-			count++;
-		}
-	}
-	return count;
-}
-
 void Render_World_Window::draw()
 {
 	static u32 render_type_index = 0;
@@ -622,8 +603,6 @@ void Render_World_Window::draw()
 		voxels.reserve(render_world->voxel_grid.width * render_world->voxel_grid.height * render_world->voxel_grid.width);
 		memset((void *)voxels.items, 0, voxels.get_size());
 		render_world->voxels_sb.read(&voxels);
-
-		auto x = how_many(voxels);
 
 		if (!voxels.is_empty()) {
 			auto matrix = make_look_to_matrix(render_world->voxel_grid_center, Vector3::base_z);
--- src/libs/utils.h
+++ src/libs/utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_FUNCTION_H
 #define UTILS_FUNCTION_H
 
+#include "number_types.h"
+#include "math/structures.h"
 
 template <typename T>
 inline bool in_range(const T &start, const T &end, const T &value)
@@ -8,5 +10,17 @@ inline bool in_range(const T &start, const T &end, const T &value)
 	return (start <= value) && (value <= end);
 }
 
+inline Point_u32 convert_1d_to_3d_index(u32 one_dimensional_index, u32 height, u32 depth)
+{
+	u32 i = one_dimensional_index / (height * depth);
+	u32 j = (one_dimensional_index % (height * depth)) / depth;
+	u32 k = one_dimensional_index % depth;
+	return Point_u32(i, j, k);
+}
+
+inline u32 convert_3d_to_1d_index(u32 x, u32 y, u32 z, u32 width, u32 depth)
+{
+	return x *(width * depth) + y * depth + z;
+}
 #endif 
 
