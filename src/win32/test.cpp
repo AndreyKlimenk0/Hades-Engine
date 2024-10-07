1f4d60f194fae408f186f0f878eabe45696ac51b
Andrey <deadshot271998@gmail.com>
2024-10-07 23:33:40 +0300

    Implement basic functionality GUI tree

--- src/win32/test.cpp
+++ src/win32/test.cpp
@@ -1,10 +1,35 @@
 #include "test.h"
 #include "../libs/str.h"
 #include "../sys/sys.h"
-#include "../libs/str.h"
 #include "../libs/number_types.h"
-#include "../libs/math/vector.h"
-#include "../libs/color.h"
+#include "../libs/structures/tree.h"
+
+inline u32 pack_RGB(const Vector3 &rgb_value)
+{
+	u32 r = u32(255.0f * rgb_value.x);
+	u32 g = u32(255.0f * rgb_value.y);
+	u32 b = u32(255.0f * rgb_value.z);
+
+	u32 result = 0;
+	result |= r << 24;
+	result |= g << 16;
+	result |= b << 8;
+	result |= 0xff;
+	return result;
+}
+
+inline u32 encode_color(const Vector3 &rgb_value)
+{
+	u32 r = u32(255.0f * rgb_value.x);
+	u32 g = u32(255.0f * rgb_value.y);
+	u32 b = u32(255.0f * rgb_value.z);
+
+	u32 result = 0;
+	result |= r << 16;
+	result |= g << 8;
+	result |= b;
+	return result;
+}
 
 void test()
 {
