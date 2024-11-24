5d19e12d54d7b2a10b580b24f7f21b22d9bbf8e7
Andrey <deadshot271998@gmail.com>
2024-11-24 16:04:44 +0200

    Add VTune solution configuration and C++ VTune API for profiling

---
+++ src/sys/profiling.h
@@ -0,0 +1,18 @@
+#ifndef PROFILING_H
+#define PROFILING_H
+
+#ifdef VTUNE_PROFILING
+#include <ittnotify.h>
+__itt_domain *get_default_domain();
+#define BEGIN_TASK(task_name) __itt_task_begin(get_default_domain(), __itt_null, __itt_null, __itt_string_handle_create(task_name))
+#define END_TASK() __itt_task_end(get_default_domain());
+#define BEGIN_FRAME() __itt_frame_begin_v3(get_default_domain(), NULL)
+#define END_FRAME() 	__itt_frame_end_v3(get_default_domain(), NULL);
+#else
+#define BEGIN_TASK(task_name)
+#define END_TASK()
+#define BEGIN_FRAME()
+#define END_FRAME()
+#endif
+
+#endif
